fbf5681f501cdb52582824e1279d4a78256c49c0
FloCorvalan <florencia.corvalan@usach.cl>
2022-04-07 00:08:03 -0400

    pruebas

--- src/apps/github/methods.py
+++ src/apps/github/methods.py
@@ -1,6 +1,38 @@
+############################ ACTIVIDADES ############################
+TEST = ['/test', '.test', '/tests']
+CODE = ['.jar', '.py', '.java', '.php', '.vue', '.js', '.r', '.jsx', '.ts', '.go']
+
+DIC = {
+    "TEST":"TEST",
+    "CODE":"CODE",
+}
+
+#####################################################################
+
+
 if __package__ is None or __package__ == '':
     # uses current directory visibility
     from ...database.database import mongo
 else:
     # uses current package visibility
     from database.database import mongo
+
+from github import Github
+from bson.objectid import ObjectId
+
+
+def get_authenticated_user(source_id):
+    source = mongo.db.get_collection('source').find_one({'_id': ObjectId(source_id)})
+    token = source['token']
+    user = source['user']
+    g = Github(token)
+    return g.get_user(user)
+
+def get_registers(team_id, source_id):
+    source = mongo.db.get_collection('source').find_one({'_id': ObjectId(source_id)})
+    team = mongo.db.get_collection('team').find_one({'_id': ObjectId(team_id)})
+    github_info = mongo.db.get_collection('github_info').find_one({'team_id': team_id, 'source_id': source_id})
+    repo_name = source['name']
+    case_id = team['actual_case_id']
+    user = get_authenticated_user(source_id)
+
--- src/apps/github/views.py
+++ src/apps/github/views.py
@@ -1,8 +1,28 @@
-from flask import Blueprint
+from fileinput import filename
+from sre_constants import BRANCH
+from traceback import print_tb
+from flask import Blueprint, Response
 from .methods import *
+import json
 
 github = Blueprint('github', __name__)
 
-@github.route('/github', methods=['GET'])
+@github.route('/github-test', methods=['GET'])
 def test():
-    return
+    user = get_authenticated_user("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")
+    repo = user.get_repo("tesis")
+    print(repo)
+    commits = repo.get_commits()
+    for commit in commits:
+        print(commit.commit.message)
+        print(commit.commit.author.name)
+        for file in commit.files:
+            print(file.filename)
+        code_list = ['.jar', '.py', '.java', '.php', '.vue', '.js', '.r', '.jsx', '.ts', '.go']
+        for end in code_list:
+            if file in commit.files:
+                file.filename.endswith(end)
+                print("FUNCIONA")
+                return 1
+    response = json.dumps(commits)
+    return Response(response, 'application/json')
